5bf8a8db74f4eeab88bac6f30d430100a9fbc7fd
serynabatov <serynabatov@yandex.ru>
2021-11-06 18:58:31 +0300

    Flask started

---
+++ backend/app.py
@@ -0,0 +1,119 @@
+import os
+import ast
+import re
+from functools import wraps
+
+from dotenv import load_dotenv, find_dotenv
+
+
+from flask import Flask, g, request, abort, request_started
+from flask_cors import CORS
+from flask_json import FlaskJSON, json_response
+from flask_restful_swagger_2 import Api, swagger, Schema
+
+from neo4j import GraphDatabase, basic_auth
+
+"""
+The credits are 
+https://github.com/neo4j-examples/neo4j-movies-template/blob/master/flask-api/app.py
+"""
+
+# For environmental variables that represents the connection
+load_dotenv(find_dotenv())
+
+app = Flask(__name__)
+
+CORS(app)
+FlaskJSON(app)
+
+api = Api(app, title='Neo4J Covid Free Demo API', api_version='0.0.10')
+
+
+@api.representation('application/json')
+def output_json(data, code, headers=None):
+    return json_response(data_=data, headers_=headers, status_=code)
+
+
+def env(key, default=None, required=True):
+    """
+    Retrieves environment variables and returns Python natives. The (optional)
+    default will be returned if the environment variable does not exist.
+    """
+    try:
+        value = os.environ[key]
+        return ast.literal_eval(value)
+    except(SyntaxError, ValueError):
+        return value
+    except KeyError:
+        if default or not required:
+            return default
+        raise RuntimeError("Missing required environment variable '%s'" % key)
+
+
+DATABASE_USERNAME = env('COVID_FREE_DATABASE_USERNAME')
+DATABASE_PASSWORD = env('COVID_FREE_DATABASE_PASSWORD')
+DATABASE_URL = env('COVID_FREE_DATABASE_URL')
+
+driver = GraphDatabase.driver(DATABASE_URL, auth=basic_auth(DATABASE_USERNAME, str(DATABASE_PASSWORD)))
+
+app.config['SECRET_KEY'] = env('SECRET_KEY')
+
+
+def get_db():
+    if not hasattr(g, 'neo4j_db'):
+        g.neo4j_db = driver.session()
+    return g.neo4j_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'neo4j_db'):
+        g.neo4j_db.close()
+
+
+def set_user(sender, **extra):
+    auth_header = request.headers.get('Authorization')
+    if not auth_header:
+        g.user = {'id': None}
+        return
+    match = re.match(r'^Token (\S+)', auth_header)
+    if not match:
+        abort(401, 'invalid authorization format. Follow `Token <token>`')
+        return
+    token = match.group(1)
+
+    # get users from the system
+    def get_user_by_token(tx, token):
+        return tx.run(
+            '''
+            MATCH (user: User {api_key: $api_key}) RETURN user
+            ''', {'api_key': token}
+        ).single()
+
+    db = get_db()
+    result = db.read_transaction(get_user_by_token, token)
+
+    try:
+        g.user = result['user']
+    except (KeyError, TypeError):
+        abort(401, 'invalid authorization key')
+    return
+
+
+request_started.connect(set_user, app)
+
+
+def login_required(f):
+
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        auth_header = request.headers.get('Authorization')
+        if not auth_header:
+            return {'message': 'no authorization provided'}, 401
+        return f(*args, **kwargs)
+
+
+
+
+
+
